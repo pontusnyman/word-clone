ddf1d1fa9567fea61925b9f0a06ad78d2ff1d084
Pontus Nyman <17062233+pontusnyman@users.noreply.github.com>
2023-03-25 21:13:10 +0100

    ex1 and ex2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessInput from '../GuessInput/GuessInput';
+import GuessResults from '../GuessResults/GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +11,16 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(guess) {
+   setGuesses([...guesses, guess]);
+  }
+  
+ return <>
+  <GuessResults guesses={guesses} />
+  <GuessInput handleSubmitGuess={handleSubmitGuess}/>
+  </>;
 }
 
 export default Game;
